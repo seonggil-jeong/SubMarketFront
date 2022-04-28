c032d57dd2ed1a6d6bda8f714cb610a1470b7c92
JeongSeonggil <dataofsg02@gmail.com>
2022-04-29 00:03:30 +0900

    Add Controller

---
+++ src/main/java/com/submarket/front/controller/item/ItemPageController.java
@@ -0,0 +1,9 @@
+package com.submarket.front.controller.item;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class ItemPageController {
+}
---
+++ src/main/java/com/submarket/front/controller/user/UserPageController.java
@@ -0,0 +1,9 @@
+package com.submarket.front.controller.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@Slf4j
+public class UserPageController {
+}

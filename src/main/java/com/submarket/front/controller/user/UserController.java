e8a7fc3f0c7b6f1490b9974187708b9320533785
JeongSeonggil <dataofsg02@gmail.com>
2022-04-29 00:26:11 +0900

    Commit

--- src/main/java/com/submarket/front/controller/user/UserController.java
+++ src/main/java/com/submarket/front/controller/user/UserController.java
@@ -2,8 +2,28 @@ package com.submarket.front.controller.user;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @Slf4j
 public class UserController {
+
+    @RequestMapping("/user/info/user-info")
+    public String userInfo(HttpServletResponse response) {
+        return "/user/info/user_info";
+    }
+
+    @RequestMapping("/user/main/user-main")
+    public String userMain() {
+        return "/user/main/user_main";
+    }
+
+    @RequestMapping("/user/setting/user-setting")
+    public String userSetting() {
+        return "/user/setting/user_setting";
+    }
 }

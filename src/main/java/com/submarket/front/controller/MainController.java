2b6e245b09944d5e2758f40db17f12d794b96045
JeongSeonggil <dataofsg02@gmail.com>
2022-04-28 23:43:24 +0900

    Initial commit

---
+++ src/main/java/com/submarket/front/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.submarket.front.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class MainController {
+
+    @RequestMapping("index")
+    public String index() {
+
+        return "/index";
+
+    }
+}
